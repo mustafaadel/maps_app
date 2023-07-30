bf8f5dfbbb45579df85c546c3927cd1a5bc4c333
Mustafa Adel Zakaria <tifaadel3@gmail.com>
2023-07-30 02:22:22 -0600

    .

--- lib/business_logic/cubit/phone_auth/phone_auth_cubit.dart
+++ lib/business_logic/cubit/phone_auth/phone_auth_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meta/meta.dart';
 
 part 'phone_auth_state.dart';
 
 class PhoneAuthCubit extends Cubit<PhoneAuthState> {
   PhoneAuthCubit() : super(PhoneAuthInitial());
+  String? verificationId;
+
+  Future<void> submitPhoneNumber(String phoneNumber)async{
+    emit(PhoneAuthLoading());
+    await FirebaseAuth.instance.verifyPhoneNumber
+    (phoneNumber: phoneNumber,
+    verificationCompleted: verificationCompleted,
+    verificationFailed: verificationFailed,
+    codeSent: codeSent,
+    codeAutoRetrievalTimeout: codeAutoRetrievalTimeout)
+  }
 }
 
